5d8132f5057b2575f579ebc45c179658f9e7e119
lp <89576294@qq.com>
2016-06-21 01:37:32 +0800

    codis

--- zookeeper/testrun.sh
+++ zookeeper/testrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -tid --net=host -e SERVER_ID=1 -e ZOOKEEPER_PORT=2181 \
+docker run -tid --net=host -p 2181:2181 -e SERVER_ID=1 -e ZOOKEEPER_PORT=2181 \
 -e ZOOKEEPER_1=localhost:2887:3887 \
 -e ZOOKEEPER_2=localhost:2888:3888 \
 -e ZOOKEEPER_3=localhost:2889:3889 \
@@ -7,14 +7,14 @@ registry.aliyuncs.com/glodonedu/zookeeper:3.4
 
 
 
-docker run -tid --net=host -e SERVER_ID=2 -e ZOOKEEPER_PORT=2182 \
+docker run -tid --net=host -p 2182:2182 -e SERVER_ID=2 -e ZOOKEEPER_PORT=2182 \
 -e ZOOKEEPER_1=localhost:2887:3887 \
 -e ZOOKEEPER_2=localhost:2888:3888 \
 -e ZOOKEEPER_3=localhost:2889:3889 \
 registry.aliyuncs.com/glodonedu/zookeeper:3.4 
 
 
-docker run -tid --net=host -e SERVER_ID=3 -e ZOOKEEPER_PORT=2183 \
+docker run -tid --net=host -p 2183:2183 -e SERVER_ID=3 -e ZOOKEEPER_PORT=2183 \
 -e ZOOKEEPER_1=localhost:2887:3887 \
 -e ZOOKEEPER_2=localhost:2888:3888 \
 -e ZOOKEEPER_3=localhost:2889:3889 \
