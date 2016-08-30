13ae4eb84a5d120dd2c5aa5e8a42c523cd9e8b63
lp <89576294@qq.com>
2016-08-30 16:02:53 +0800

    集成oneapm

--- alitomcat/README.md
+++ alitomcat/README.md
@@ -4,3 +4,8 @@
 * 使用环境变量LOCAL_SYNC_DIR指定在本地环境中需要同步的目录
 * 使用环境变量${app_profile}_HOST_PWD 和${app_profile}_HOST_DIR分别指定远程主机密码和路径，其中${app_profile}_HOST_DIR规则为username@remoteip:dir
 * 可以指定不超过20个的${app_profile}_HOST_PWD_$i 和 ${app_profile}_HOST_DIR_$i，其中i的值为1~20 
+
+##集成oneapm的监控功能
+* 通过supervisord控制oneapm的开启或停止。默认supervisor的oneapm配置文件为supervisor_ci.conf.off，在启动docker容器之后，需要执行docker exec删除文件后缀off
+* 使用oneapm需要同步当前时间，默认使用ntpupdate执行更新
+* 使用sysstat监控cup和io指标 
--- alitomcat/testrun.sh
+++ alitomcat/testrun.sh
@@ -2,9 +2,12 @@ docker stop cloudinsight-agent
 
 docker rm cloudinsight-agent
 
-docker run -dti --name cloudinsight-agent -h hello2 \
+docker run -dti --name cloudinsight-agent -h helloworld -p 8081:8080 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v /proc/:/host/proc/:ro \
 	-v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
-	-e LICENSE_KEY={your_license_key_here} \
-	registry.aliyuncs.com/glodonedu/alitomcat:1.1
+	registry.aliyuncs.com/glodonedu/alitomcat:0.7
+
+#开启监控
+docker exec cloudinsight-agent mv /etc/supervisor.conf.d/supervisor_ci.conf.off /etc/supervisor.conf.d/supervisor_ci.conf
+
